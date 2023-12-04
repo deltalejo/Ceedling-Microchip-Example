0d4a5ccabb0fce760a9e8faa4358dac2d766dd30
Alejandro Rosso <deltalejo@gmail.com>
2023-12-04 07:33:18 -0500

    Add rakefile to support multiple platforms and targets.

---
+++ rakefile.rb
@@ -0,0 +1,36 @@
+require('ceedling')
+
+# Folder containing hardware variants
+TARGETS_PATH = 'targets'.freeze
+
+# Set defaults
+DEFAULT_PLATFORM = 'host'.freeze
+DEFAULT_TARGET = 'PIC16F1936'.freeze
+
+platform = DEFAULT_PLATFORM
+target = DEFAULT_TARGET.upcase()
+
+# Get platform and target from arguments
+ARGV&.each do |arg|
+	case arg
+	when /^platform:(\w+)/
+		platform = $1
+	when /^target:(\w+)/
+		target = $1.upcase()
+	end
+end
+
+PLATFORM = platform
+TARGET = target
+
+# Tell Ceedling to load selected platform and target configuration files
+ENV['CEEDLING_USER_PROJECT_FILE'] = "#{PLATFORM}.yml"
+ENV['CEEDLING_MIXIN_PROJECT_FILES'] = File.join(TARGETS_PATH, "#{TARGET}.yml")
+
+# Load Ceedling project
+Ceedling.load_project
+
+# Dummy tasks to stop Ceedling complaining about unknown tasks when giving
+# platform:<platform> and/or target:<target> arguments
+rule /^platform.*/
+rule /^target.*/
