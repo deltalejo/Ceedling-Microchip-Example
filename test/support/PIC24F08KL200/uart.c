22b8a39f5782acfaa9cfc54cf5a129931e27473d
Alejandro Rosso <deltalejo@gmail.com>
2023-12-10 12:16:24 -0500

    Add initial support for target device PIC24F08KL200.

---
+++ test/support/PIC24F08KL200/uart.c
@@ -0,0 +1,29 @@
+#include <xc.h>
+
+#include "uart.h"
+
+static inline void poll_tx_complete(void)
+{
+	
+}
+
+static void uart_output_complete()
+{
+	NOP();
+}
+
+void uart_start(void)
+{
+	
+}
+
+void uart_end(void)
+{
+	
+}
+
+int uart_putchar(int c)
+{
+	
+	return c;
+}
