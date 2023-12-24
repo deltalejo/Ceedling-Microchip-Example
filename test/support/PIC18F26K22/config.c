9f108acc7e9213c21a64cc4c32370f5a84aa6db5
Alejandro Rosso <deltalejo@gmail.com>
2023-12-24 10:41:19 -0500

    Update PIC18F26K22 test support for running test on actual hardware at maximum speed.

--- test/support/PIC18F26K22/config.c
+++ test/support/PIC18F26K22/config.c
@@ -1,2 +1,49 @@
-// Disable extended instruction
+#pragma config FOSC = INTIO67
+#pragma config PLLCFG = OFF
+#pragma config PRICLKEN = OFF
+#pragma config FCMEN = OFF
+#pragma config IESO = OFF
+
+#pragma config PWRTEN = OFF
+#pragma config BOREN = OFF
+#pragma config BORV = 285
+
+#pragma config WDTEN = OFF
+#pragma config WDTPS = 1
+
+#pragma config CCP2MX = PORTC1
+#pragma config PBADEN = ON
+#pragma config CCP3MX = PORTB5
+#pragma config HFOFST = OFF
+#pragma config T3CMX = PORTC0
+#pragma config P2BMX = PORTB5
+#pragma config MCLRE = EXTMCLR
+
+#pragma config STVREN = ON
+#pragma config LVP = ON
 #pragma config XINST = OFF
+#pragma config DEBUG = ON
+
+#pragma config CP0 = OFF
+#pragma config CP1 = OFF
+#pragma config CP2 = OFF
+#pragma config CP3 = OFF
+
+#pragma config CPB = OFF
+#pragma config CPD = OFF
+
+#pragma config WRT0 = OFF
+#pragma config WRT1 = OFF
+#pragma config WRT2 = OFF
+#pragma config WRT3 = OFF
+
+#pragma config WRTC = OFF
+#pragma config WRTB = OFF
+#pragma config WRTD = OFF
+
+#pragma config EBTR0 = OFF
+#pragma config EBTR1 = OFF
+#pragma config EBTR2 = OFF
+#pragma config EBTR0 = OFF
+
+#pragma config EBTRB = OFF
--- test/support/PIC18F26K22/uart.c
+++ test/support/PIC18F26K22/uart.c
@@ -1,36 +1,49 @@
+#include <stdbool.h>
+
 #include <xc.h>
 
 #include "uart.h"
 
-static inline void poll_tx_complete(void)
-{
-	while (!PIR1bits.TXIF);
-}
-
-static void uart_output_complete()
-{
-	NOP();
-}
-
 void uart_start(void)
 {
+	#ifndef SIMULATOR
+	while (!OSCCONbits.HFIOFS);
+	#endif
+	
+	OSCCONbits.IRCF = 7;
+	OSCTUNEbits.PLLEN = 1;
+	
+	#ifndef SIMULATOR
+	while (!OSCCON2bits.PLLRDY);
+	#endif
+	
+	BAUDCONbits.BRG16 = 1;
+	TXSTAbits.BRGH = 1;
+	SPBRG = 138;
+	
 	RCSTAbits.SPEN = 1;
 	TXSTAbits.TXEN = 1;
 }
 
-void uart_end(void)
+void uart_stop(void)
 {
-	poll_tx_complete();
+	while (!TXSTAbits.TRMT);
+	
 	TXSTAbits.TXEN = 0;
 	RCSTAbits.SPEN = 0;
-	uart_output_complete();
+	
+	uart_end();
+}
+
+void uart_end(void)
+{
+	while (true);
 }
 
 int uart_putchar(int c)
 {
+	while (!PIR1bits.TXIF);
 	TXREG = (unsigned char) c;
-	NOP();
-	poll_tx_complete();
 	return c;
 }
 
