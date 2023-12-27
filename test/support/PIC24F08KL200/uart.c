bbc9fdf9940c8645920cc14a34ec1c811de54937
Alejandro Rosso <deltalejo@gmail.com>
2023-12-26 22:04:56 -0500

    Update PIC24F08KL200 test support for running test on actual hardware at maximum speed.

--- test/support/PIC24F08KL200/uart.c
+++ test/support/PIC24F08KL200/uart.c
@@ -1,29 +1,39 @@
+#include <stdbool.h>
+
 #include <xc.h>
 
 #include "uart.h"
 
-static inline void poll_tx_complete(void)
-{
-	
-}
-
-static void uart_output_complete()
-{
-	NOP();
-}
-
 void uart_start(void)
 {
+	CLKDIVbits.RCDIV = 0;
 	
+	U1MODEbits.BRGH = 1;
+	U1BRG = 34;
+	
+	U1MODEbits.UARTEN = 1;
+	U1STAbits.UTXEN = 1;
+}
+
+void uart_stop(void)
+{
+	while (!U1STAbits.TRMT);
+	
+	U1STAbits.UTXEN = 0;
+	U1MODEbits.UARTEN = 0;
+	
+	uart_end();
 }
 
 void uart_end(void)
 {
-	
+	while (true);
 }
 
 int uart_putchar(int c)
 {
-	
+	while (!IFS0bits.U1TXIF);
+	IFS0bits.U1TXIF = 0;
+	U1TXREG = c;
 	return c;
 }
